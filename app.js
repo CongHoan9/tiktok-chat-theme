eddfa5a0c751b1b9185881e154f1d7b38b51833f
CongHoan9 <158456623+CongHoan9@users.noreply.github.com>
2026-03-19 19:20:21 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -9,3 +9,21 @@ function backToChat() {
     settingPage.style.display = "none"; 
     chatPage.style.display = "block";
 }
+
+const inputBox = document.getElementById('input-textbox');
+const imageBtn = document.getElementById('image-button');
+const voiceBtn = document.getElementById('voice-button');
+const stickerBtn = document.getElementById('sticker-button');
+const enterBtn = document.getElementById('enter-button');
+
+inputBox.addEventListener('input', () => {
+    if (inputBox.value.trim() !== '') {
+        imageBtn.classList.add('hidden');
+        voiceBtn.classList.add('hidden');
+        enterBtn.classList.remove('hidden');
+    } else {
+        imageBtn.classList.remove('hidden');
+        voiceBtn.classList.remove('hidden');
+        enterBtn.classList.add('hidden');
+    }
+});
